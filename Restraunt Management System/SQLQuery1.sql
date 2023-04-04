b81820523a752cd2beed23706eb73225fa20f42a
ImmortalComputer <125343392+cedwincode@users.noreply.github.com>
2023-04-04 18:24:54 +0530

    Delete Restraunt Management System directory

--- Restraunt Management System/SQLQuery1.sql
+++
@@ -1,7 +0,0 @@
-﻿SELECT * FROM Bill;
-UPDATE Bill  SET Id =1 WHERE Id = null;
-INSERT INTO Bill ([Food type],[Food qty],[Drink type],[Drink qty],[Discount],[Total],[Datetime],[B_id]) VALUES('kaju',5,'soda',2,0,7,'2023-03-29',1);
-
-DELETE FROM Bill WHERE ID BETWEEN 1 AND 4;
-SELECT MAX(Id) FROM Bill;
-
